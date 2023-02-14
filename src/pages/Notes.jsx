f9e011ca6bfbdc1d7893feaa2896c3e283c9cc99
JOEX <oyinjoe23@gmail.com>
2023-02-14 21:49:23 +0100

    add: search bar filtering funtionality

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -2,19 +2,34 @@ import { CiSearch } from "react-icons/ci"
 import { BsPlusLg } from "react-icons/bs"
 import { Link } from "react-router-dom"
 import NoteItem from "../components/NoteItem"
-
+import { useState } from "react"
 
 const Notes = ({notes}) => {
+  const [showSearch, setShowSearch] = useState(false);
+  const [text, setText] = useState('');
+  const [filteredNotes, setFilteredNotes] = useState(notes);
+
+
+  const handleSearch = () =>{
+    setFilteredNotes(notes.filter(note => {
+      if(note.title.toLowerCase().match(text.toLowerCase())){
+        return note;
+      }
+    }))
+  }
+
+  useEffect(handleSearch, [text])
+
     return(
        <section>
         <header className="notes__header">
-            <h2>My Note</h2>
-            {/* <input type='text' autofocus placeholder="Keyword..." /> */}
-            <button className='btn'><CiSearch/></button>
+           {!showSearch && <h2>My Note</h2>}
+            { showSearch && <input type='text' value={text} onchange={(e) => {setText(e.target.value);handleSearch();}} autofocus placeholder="Keyword..." /> }
+            <button className='btn' onclick={() => setShowSearch(prevState => !prevState)}><CiSearch/></button>
         </header>
         <div className="notes__container">
          {
-           notes.map(note => <NoteItem key={note.id} note= {note} />)
+           filteredNotes.map(note => <NoteItem key={note.id} note= {note} />)
          }
         </div>
         <Link to="/create-note" className="btn add__btn"><BsPlusLg/></Link>
